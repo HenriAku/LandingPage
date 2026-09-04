9b0865a5c2871b2404abff439e6b4f687cd4b44e
Henri <rougeollehenri@gmail.com>
2026-09-04 17:37:25 +0200

    modif next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export', // 👈 AJOUTEZ CETTE LIGNE ABSOLUMENT
+  images: {
+    unoptimized: true, // 👈 AJOUTEZ CELA AUSSI (évite les erreurs avec les images sur Netlify)
+  },
 };
 
 export default nextConfig;
